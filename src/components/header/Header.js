a4457a8256298f6eb538755a38994b50a41afcde
NoraSaffar <norasaffar8910@gmail.com>
2024-01-09 11:06:21 +0000

    enhancemnets: add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+        <div className="header-inner-container">
+            <h1>Awesome Header</h1>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
